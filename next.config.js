55c78d1f2b8550b9803c4acc3bdd979a76aa4a6b
ay0o5h <aayosh553@gmail.com>
2022-03-20 14:47:08 +0300

    some changes

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = {nextConfig}
+module.exports = {
+  nextConfig,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'ar'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+  }
+}
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import ProgressBar from "@badrap/bar-of-progress";
 import 'antd/dist/antd.css';
 import "devextreme/dist/css/dx.light.css";
+import { NextIntlProvider } from 'next-intl';
 import Router from "next/router";
 // import 'scheduler-react/lib/css/style.css';
 import Layout from '../components/layout/layout';
@@ -16,9 +17,11 @@ Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 function MyApp({ Component, pageProps }) {
   return (
+     <NextIntlProvider messages={pageProps.messages}>
      <Layout>
       <Component {...pageProps} />
-       </Layout>
+      </Layout>
+      </NextIntlProvider>
       )
 }
 
